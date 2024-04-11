b1d105aaeb77c99c52b50782752e56640af3c454
5nam <ohnam90@gmail.com>
2024-04-12 00:23:13 +0900

    [test] : 중복된 save 코드가 동시성 제어가 안되어서 @Sql 로 해결

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -4,6 +4,7 @@ import com.example.demo.model.UserStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Optional;
 
@@ -11,41 +12,34 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest(showSql = true)
+@Sql("/sql/user-repository-test-data.sql")
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
-    @Test
-    void UserRepository_가_제대로_연결되었다() { // jpa 가 h2 랑 제대로 연동되는지 확인
-        // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setEmail("ohnam00@naver.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("lua");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaa-aaaaa");
-
-        // when
-        UserEntity result = userRepository.save(userEntity);
-
-        // then
-        assertThat(result.getId()).isNotNull(); // 저장이 잘 되어 있다면, id 가 할당되어 있을 것!
-    }
+//    @Test
+//    void UserRepository_가_제대로_연결되었다() { // jpa 가 h2 랑 제대로 연동되는지 확인
+//        // given
+//        UserEntity userEntity = new UserEntity();
+//        userEntity.setEmail("ohnam00@naver.com");
+//        userEntity.setAddress("Seoul");
+//        userEntity.setNickname("lua");
+//        userEntity.setStatus(UserStatus.ACTIVE);
+//        userEntity.setCertificationCode("aaaaa-aaaaa");
+//
+//        // when
+//        UserEntity result = userRepository.save(userEntity);
+//
+//        // then
+//        assertThat(result.getId()).isNotNull(); // 저장이 잘 되어 있다면, id 가 할당되어 있을 것!
+//    }
 
     @Test
     void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
         // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("ohnam00@naver.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("lua");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaa-aaaaa");
 
         // when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
 
         // then
@@ -55,16 +49,8 @@ class UserRepositoryTest {
     @Test
     void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
         // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("ohnam00@naver.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("lua");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaa-aaaaa");
 
         // when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
 
         // then
@@ -74,16 +60,8 @@ class UserRepositoryTest {
     @Test
     void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() {
         // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("ohnam00@naver.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("lua");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaa-aaaaa");
 
         // when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByEmailAndStatus("ohnam00@naver.com", UserStatus.ACTIVE);
 
         // then
@@ -93,16 +71,8 @@ class UserRepositoryTest {
     @Test
     void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
         // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("ohnam00@naver.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("lua");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaa-aaaaa");
 
         // when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByEmailAndStatus("ohnam00@naver.com", UserStatus.PENDING);
 
         // then
