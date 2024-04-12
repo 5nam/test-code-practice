a93f9c8ba62d846e673c07431a64f11313d37ae8
5nam <ohnam90@gmail.com>
2024-04-12 19:23:47 +0900

    [test] : UserService 테스트 코드 추가

---
+++ src/test/resources/sql/delete-all-data.sql
@@ -0,0 +1,2 @@
+delete `posts` where 1;
+delete `users` where 1;
--- src/test/resources/sql/user-repository-test-data.sql
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -1,2 +1,2 @@
 insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-values (1, 'ohnam00@naver.com', 'kok202', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+values (1, 'ohnam00@naver.com', 'ohnam', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
