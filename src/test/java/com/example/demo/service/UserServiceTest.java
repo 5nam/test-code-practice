1c0c3ae54461de3b2a85452a14adb418a73d3acf
5nam <ohnam90@gmail.com>
2024-04-12 22:13:42 +0900

    [test] : UserCreate 기능 테스트 코드 추가 변경 사항 1) save 코드 실행 시 id 인덱스 1부터 할당하여, primary key 충돌 오류 발생, 그래서 sql 문 id 를 3, 4 로 바꾸어줌 추가 변경해야 할 사항 1) delete-all-data.sql 이 메서드마다 잘 동작하면 문제 없을텐데, 어떤 곳이 문제인지 확인해봐야 할 듯

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -1,10 +1,17 @@
 package com.example.demo.service;
 
 import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.UserCreateDto;
 import com.example.demo.repository.UserEntity;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
@@ -12,6 +19,7 @@ import org.springframework.test.context.jdbc.SqlGroup;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 @TestPropertySource("classpath:test-application.properties")
@@ -23,6 +31,8 @@ class UserServiceTest {
 
     @Autowired
     private UserService userService;
+    @MockBean // 스프링에 있는 JavaMailSender 라는 Bean 객체를 Mock 으로 선언된 객체로 덮어쓰기 하는 것 -> 테스트 실행 시 MockBean 값이 주입되어 실행
+    private JavaMailSender mailSender;
 
     @Test
     void getByEmail_은_ACTIVE_상태인_유저를_찾아올_수_있다() {
@@ -52,7 +62,7 @@ class UserServiceTest {
     void getById_은_ACTIVE_상태인_유저를_찾아올_수_있다() {
         // given
         // when
-        UserEntity result = userService.getById(1);
+        UserEntity result = userService.getById(3);
 
         // then
         assertThat(result.getNickname()).isEqualTo("ohnam");
@@ -64,7 +74,30 @@ class UserServiceTest {
         // when
         // then
         assertThatThrownBy(() -> {
-            UserEntity result = userService.getById(2);
+            UserEntity result = userService.getById(4);
         }).isInstanceOf(ResourceNotFoundException.class);
     }
+
+    @Test
+    void userCreateDto_를_이용하면_유저를_생성할_수_있다() {
+
+        // given
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+                .email("ohnam00@naver.com")
+                .address("Seoul")
+                .nickname("ohnam")
+                .build();
+
+        BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
+        // when
+        UserEntity result = userService.create(userCreateDto);
+
+        // then
+        assertThat(result.getId()).isNotNull(); // id 가 잘 생성되었는지 보는 것
+        assertThat(result.getStatus()).isEqualTo(UserStatus.PENDING); // 처음 회원 가입하면, PENDING 상태인 것
+        // 랜덤 값도 잘 만들어졌는지 보고 싶은데, UUID 랜덤 값을 테스트할 방법이 없음
+        // assertThat(result.getCertificationCode()).isEqualTo("T.T");
+    }
+
 }
--- src/test/resources/sql/delete-all-data.sql
+++ src/test/resources/sql/delete-all-data.sql
@@ -1,2 +1,3 @@
-delete `posts` where 1;
-delete `users` where 1;
+
+delete from `posts` where 1;
+delete from `users` where 1;
