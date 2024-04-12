9f184628948b017ea5aa1c4330c703258bbb3a42
5nam <ohnam90@gmail.com>
2024-04-12 23:47:00 +0900

    [test] : UserController 테스트 코드 추가

---
+++ src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,5 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (3, 'ohnam00@naver.com', 'ohnam', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (4, 'ohnam01@naver.com', 'ohnam01', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
